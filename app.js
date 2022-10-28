0be21bcd9c2bb2c4728f32dfb69cc3b0e3406611
komzibaby <114658286+komzibaby@users.noreply.github.com>
2022-10-28 04:16:19 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+let matrix = [[5,7],[6,5]];
+var determinant =0;
+var i=0;
+var j=0;
+  for(i=0;i<2;i++){
+  for(j=0;j<2;j++){
+  determinant= (matrix[0][0]*matrix[1][1])-(matrix[0][1]*matrix[1][0])
+
+  }
+}
+console.log(determinant)
